6567ea51d017b00746cef5c26c2d2687ac7bda85
harshisjelly2010 <142248356+harshisjelly2010@users.noreply.github.com>
2023-08-14 17:06:25 +0100

    Add files via upload

---
+++ roll-a-dice.py
@@ -0,0 +1,18 @@
+import random
+
+y = yes()
+n = no()
+
+input("Do you want to roll a dice")
+if (input == y){
+no = radom.randint(1,6)
+conditiput.on()
+}
+
+input("do you wish to roll again? y or n")
+if(input == y){
+repeat()
+}
+else{
+    exit()
+}
